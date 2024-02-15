12b26011c69417b8348ae17477aa8d6bc1aa6179
Fa-collab1 <opus.ionam@gmail.com>
2024-02-15 02:15:08 +0100

    Mixin för slumpmässiga knappfärger tillagd

---
+++ src/js/js.js
@@ -0,0 +1,16 @@
+document.getElementById("duckButton").addEventListener("click", function() {
+    alert("Kvack kvack! Här är din and.");
+  });
+  
+  document.getElementById("ducksButton").addEventListener("click", function() {
+    alert("Kvack kvack! Kvack Kvack! Många änder!!! Här är dina ankor också.");
+  });
+
+  document.getElementById("helpButton").addEventListener("click", function() {
+    alert("Förstår, det är för många ankor och änder. Vi finns här för dig och kommer snart till undsättning.");
+  });
+
+  document.getElementById("geeseButton").addEventListener("click", function() {
+    alert("Wow, du är en riktig fågelvän! Här är dina gäss. De jagar bort ankor och änder. Ditt liv är nu räddat av oss och våra tappra gäss.");
+  });
+
