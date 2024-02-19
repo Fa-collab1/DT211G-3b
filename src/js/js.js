c8b6b3420ba959f619688b131d67708543f4603f
Fa-collab1 <opus.ionam@gmail.com>
2024-02-19 18:03:34 +0100

    3 animeringar är nu klara

--- src/js/js.js
+++ src/js/js.js
@@ -1,24 +1,27 @@
-let knapptexter = ["Kvack kvack! Här är din and.", "Kvack kvack! Kvack Kvack! Många änder!!! Här är dina ankor också.", "Förstår, det är för många ankor och änder. Vi finns här för dig och kommer snart till undsättning.", "Wow, du är en riktig fågelvän! Här är dina gäss. De jagar bort ankor och änder. Ditt liv är nu räddat av oss och våra tappra gäss."];
+let knapptexter = ["Kvack kvack! Här är din and.", "Kvack kvack! Kvack Kvack! Många änder!!! Här är dina ankor också.", "Förstår, det är för många ankor och änder. Vi finns här för dig och kommer snart till undsättning."];
+let texttexter = ["Nej, du ska inte trycka här! Tryck på andknapparna!", "Kvack kvack! Kvack Kvack! Önskar du dig verkligen ankor? ;)", "Behöver du undsättas? Be då om en gåsattack!"];
 
+document.getElementById("duckButton").addEventListener("click", function () {
+  alert(knapptexter[0]);
+});
 
-document.getElementById("duckButton").addEventListener("click", function() {
-    alert(knapptexter[0]);
-  });
-  
-  document.getElementById("ducksButton").addEventListener("click", function() {
-    alert(knapptexter[1]);
-  });
+document.getElementById("ducksButton").addEventListener("click", function () {
+  alert(knapptexter[1]);
+});
 
-  document.getElementById("helpButton").addEventListener("click", function() {
-    alert(knapptexter[2]);
-  });
+document.getElementById("helpButton").addEventListener("click", function () {
+  alert(knapptexter[2]);
+});
 
-  document.getElementById("geeseButton").addEventListener("click", function() {
-    alert(knapptexter[3]);
-  });
+document.getElementById("geeseButton").addEventListener("click", function () {
+  var element = document.getElementById('geese_attack');
+  element.classList.remove('change_size_animation');
 
+  setTimeout(function () {
+    element.classList.add('change_size_animation');
+  }, 0);
 
-
+});
 
 
 // Hämta alla element med klassen 'animation_text'
@@ -27,8 +30,8 @@ let elements = document.getElementsByClassName("animation_text");
 // Loopa igenom alla hämtade element
 for (let i = 0; i < elements.length; i++) {
   // Lägg till en klick-händelselyssnare till varje element
-  elements[i].addEventListener("click", function() {
-    alert(knapptexter[i]);
-    
+  elements[i].addEventListener("click", function () {
+    alert(texttexter[i]);
+
   });
 }
