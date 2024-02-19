603e37bc938621a4357146730a387649a90e3480
Fa-collab1 <opus.ionam@gmail.com>
2024-02-19 07:04:57 +0100

    2 animeringar tillagda

--- src/js/js.js
+++ src/js/js.js
@@ -1,16 +1,34 @@
+let knapptexter = ["Kvack kvack! Här är din and.", "Kvack kvack! Kvack Kvack! Många änder!!! Här är dina ankor också.", "Förstår, det är för många ankor och änder. Vi finns här för dig och kommer snart till undsättning.", "Wow, du är en riktig fågelvän! Här är dina gäss. De jagar bort ankor och änder. Ditt liv är nu räddat av oss och våra tappra gäss."];
+
+
 document.getElementById("duckButton").addEventListener("click", function() {
-    alert("Kvack kvack! Här är din and.");
+    alert(knapptexter[0]);
   });
   
   document.getElementById("ducksButton").addEventListener("click", function() {
-    alert("Kvack kvack! Kvack Kvack! Många änder!!! Här är dina ankor också.");
+    alert(knapptexter[1]);
   });
 
   document.getElementById("helpButton").addEventListener("click", function() {
-    alert("Förstår, det är för många ankor och änder. Vi finns här för dig och kommer snart till undsättning.");
+    alert(knapptexter[2]);
   });
 
   document.getElementById("geeseButton").addEventListener("click", function() {
-    alert("Wow, du är en riktig fågelvän! Här är dina gäss. De jagar bort ankor och änder. Ditt liv är nu räddat av oss och våra tappra gäss.");
+    alert(knapptexter[3]);
   });
 
+
+
+
+
+// Hämta alla element med klassen 'animation_text'
+let elements = document.getElementsByClassName("animation_text");
+
+// Loopa igenom alla hämtade element
+for (let i = 0; i < elements.length; i++) {
+  // Lägg till en klick-händelselyssnare till varje element
+  elements[i].addEventListener("click", function() {
+    alert(knapptexter[i]);
+    
+  });
+}
